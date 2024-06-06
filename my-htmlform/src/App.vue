7c1bdb12b88f4e2381e20e2ffd8b406798afd0ac
Peroxlyde <pooh.blacklight@gmail.com>
2024-06-06 18:14:13 +0700

    first

---
+++ my-htmlform/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <myForm/>
+</template>
+
+<script>
+import myForm from './components/myForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-htmlform/src/components/myForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form">
+    <h1>แบบสอบถาม</h1>
+    <form @submit.prevent="submitForm">
+      <div class="type1"></div>
+      <div v-for="(selected_question, index) in questions" :key="index">
+        <p>{{ selected_question.question_name }}</p>
+
+      </div>
+      <div class="type2"></div>
+      <div class="type5"></div>
+    </form>   
+  </div>
+</template>
+
+<script>
+let json = require('./question.json');
+  export default {
+    data() {
+      return{
+      questions: json , // เก็บข้อมูลแบบสอบถามจากไฟล์ question.json
+      userAnswersRadio: [],
+      userAnswersCheckbox: [],
+      userAnswersText: [], 
+      userAnswersOtherRadio: [],
+      userAnswersOtherCheckbox: [] 
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
